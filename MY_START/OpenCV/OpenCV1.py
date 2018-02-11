0732955bd25bd2e8c08d58c08d83d0a9029fb4cc
CoolCoder31 <hitesh.gorana@technonjr.org>
2018-02-11 18:50:53 +0530

    OpenCV example For videoCapture

--- MY_START/OpenCV/OpenCV1.py
+++ MY_START/OpenCV/OpenCV1.py
@@ -6,12 +6,13 @@ cap = cv2.VideoCapture(0)
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('outpu.avi',fourcc,20.0,(640,480))
 while(cap.isOpened()):
-    ret, frame = cap.read()#This code initiates an infinite loop
+    ret, frame = cap.read(0)#This code initiates an infinite loop
     #ret for boolean reagrading it returns or not
     gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 #OpenCV reads colors as BGR (Blue Green Red),
     # where most computer applications read as RGB
     # (Red Green Blue)
+    out.write(frame)
     cv2.imshow('FRAME',gray)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
