62ab731d47a395cb5fc2220dfbd44031076919c6
CoolCoder31 <hitesh.gorana@technonjr.org>
2018-02-12 05:42:12 +0530

    OpenCV example For Image pixel

--- MY_START/OpenCV/OpenCV3.py
+++ MY_START/OpenCV/OpenCV3.py
@@ -11,9 +11,9 @@ px = img[100:150,100:150]
 # print(px)
 img[100:150,100:150] = [255,255,255]
 # print(px)
-print(img.shape)
-print(img.size)
-print(img.dtype)
+# print(img.shape)
+# print(img.size)
+# print(img.dtype)
 watch_face = img[37:111,107:194]
 img[0:74,0:87] = watch_face
 
