aa652580aa7132ac595b6f9d2e17e742fe65ac70
CoolCoder31 <hitesh.gorana@technonjr.org>
2018-02-13 22:09:51 +0530

    AI

---
+++ HackerRANK/AritficalInteligence/Bot saves princess.py
@@ -0,0 +1,12 @@
+def displayPathtoPrincess(n, grid):
+    for nrow in range(n):
+        for ncol in range(n):
+
+
+    # print all the moves here
+m = int(input())
+grid = []
+for i in range(0, m):
+    grid.append(input().strip())
+
+displayPathtoPrincess(m, grid)
