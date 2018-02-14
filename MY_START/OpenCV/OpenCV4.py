9c61a2006018dae7f6655f9028847c9af623ce09
CoolCoder31 <hitesh.gorana@technonjr.org>
2018-02-14 21:20:38 +0530

    AI

--- MY_START/OpenCV/OpenCV4.py
+++ MY_START/OpenCV/OpenCV4.py
@@ -1,20 +1,9 @@
-import numpy as np
 import cv2
-
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
-img = cv2.imread('imput.jpg')
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    img = cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
-    roi_gray = gray[y:y+h, x:x+w]
-    roi_color = img[y:y+h, x:x+w]
-    eyes = eye_cascade.detectMultiScale(roi_gray)
-    for (ex,ey,ew,eh) in eyes:
-        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
-
-cv2.imshow('img',img)
+import numpy as np
+img = cv2.imread("input.jpg")
+# print(img.shape)
+num_rows , num_cols = img.shape[:2]
+translation_matrix = np.float32([[1,0,70],[0,1,110]])
+img_translation = cv2.warpAffine(img,translation_matrix,(num_cols + 70 + 30,num_rows +110 + 30))
+cv2.imshow("IMAGE" , img_translation)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
